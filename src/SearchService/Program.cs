d1fba143ead57551398129fea14c52c593656fb1
Itamar Axelrad <iaxelrad@gmail.com>
2025-03-22 22:58:32 +0200

    Adding Seed data

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,29 +1,28 @@
 using MongoDB.Driver;
 using MongoDB.Entities;
+using SearchService.Data;
 using SearchService.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
 app.UseAuthorization();
 
 app.MapControllers();
 
-await DB.InitAsync("SearchDb", MongoClientSettings
-.FromConnectionString(builder.Configuration.GetConnectionString("MongoDbConnection")));
-
-await DB.Index<Item>()
-.Key(x => x.Make, KeyType.Text)
-.Key(x => x.Model, KeyType.Text)
-.Key(x => x.Color, KeyType.Text)
-.CreateAsync();
+try
+{
+  await DbInitializer.InitDb(app);
+}
+catch (Exception e)
+{
+  Console.WriteLine(e);
+}
 
 app.Run();
