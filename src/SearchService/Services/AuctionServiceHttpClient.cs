d3d48d113f0d40e506613b164b0aeed5ee2355a4
Itamar Axelrad <iaxelrad@gmail.com>
2025-03-29 14:22:53 +0300

    Adding Http communication to get the data between services

---
+++ src/SearchService/Services/AuctionServiceHttpClient.cs
@@ -0,0 +1,21 @@
+using System;
+using MongoDB.Entities;
+using SearchService.Models;
+
+namespace SearchService.Services;
+
+public class AuctionServiceHttpClient(HttpClient httpClient, IConfiguration config)
+{
+  private readonly HttpClient _httpClient = httpClient;
+  private readonly IConfiguration _config = config;
+
+  public async Task<List<Item>> GetItemsForSearchDb()
+  {
+    var lastUpdated = await DB.Find<Item, string>()
+   .Sort(x => x.Descending(x => x.UpdatedAt))
+   .Project(x => x.UpdatedAt.ToString())
+   .ExecuteFirstAsync();
+
+    return await _httpClient.GetFromJsonAsync<List<Item>>(_config["AuctionServiceUrl"] + "/api/auctions?date=" + lastUpdated);
+  }
+}
